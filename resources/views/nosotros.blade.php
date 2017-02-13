fd9ce417cd4f89c68eddd4f2fd8f020ce93e8ed9
jejoreba <jeremy.reyes@openwebdevelopers.net>
2017-02-12 21:51:51 -0500

    Nosotros finalizado

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -15,22 +15,20 @@
 <div class="reservation_top">
 
           <div class="about">
-          <h3 style="color: red;">Que somos?</h3>
-          <p style="text-align: justify;">Somos un hotel que Estamos Situados en Barranquilla/Colombia en la calle 19#2.34 en todo 
-          el bulevar de Simón Bolívar, el Hotel Plaza Bolivar es uno de 
-          los hoteles de Barranquilla que mejor responden a las exigencias
-          de los huéspedes con atención y servicios de primera calidad.
+          <h3 style="color: red;">Quienes Somos?</h3>
+          <p style="text-align: justify;">
+              {{$informacionPagina[0]['nosotros']}}
          </p>
 
 
           <h3 style="color: red;">Misión</h3>
-          <p style="text-align: justify;">Ofrecerle a cada uno de nuestros clientes un servicio único con el cual ellos puedan sentirse
-          en completa tranquilidad y comodidad para que esten a gusto en nuestro hotel.</p>    
-
-           
-             <h3 style="color: red;">Visión</h3>
-          <p style="text-align: justify;">Ser catalogados como uno de los mejores hoteles de barranquilla brindandole a los clientes la calidad
-          de servicio que no puedan encontrar en cualquier otro hotel.</p>   
+          <p style="text-align: justify;">
+              {{$informacionPagina[0]['mision']}}
+          </p>
+          <h3 style="color: red;">Visión</h3>
+          <p style="text-align: justify;">
+              {{$informacionPagina[0]['vision']}}
+          </p>
   
 
           </div>
@@ -43,22 +41,13 @@
 </div>
 </div>
 
-<!-- <div class="row">
-  <div class="col-md-12">
-    <div class="about">
-     <h3 style="color: red;">Visión</h3>
-          <p style="text-align: justify;">Ser catalogados como uno de los mejores hoteles de barranquilla brindandole a los clientes la calidad
-          de servicio que no puedan encontrar en cualquier otro hotel.</p>   
-  </div>
-  </div>
-</div>  -->
 
 
 <div class="row">
   <div class="col-md-12">
     <div class="about">
      <h3 style="color: red;">Valores</h3>
-          <p> Honestidad - Empeño - Hospitalidad - Pertenencia - Trabajo en equipo.</p>   
+          <p> {{$informacionPagina[0]['valores']}}</p>
   </div>
   </div>
 </div>
